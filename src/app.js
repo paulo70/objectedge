d02aa4441be087c6b56cfb429f85770faa3ce831
paulo70 <paulofrontend@gmail.com>
2020-11-27 11:54:13 -0300

    Create app js and start projetc

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import 'modules/bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+function App(props){
+  return (
+    <div className='container'>
+      <div className='row'>
+        <div className='col-sm-12'>
+          <h1>Welcome</h1>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
